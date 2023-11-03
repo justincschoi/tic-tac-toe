b9bbe87ceec9c9cefc29288c812e72faf4874a1b
justincschoi <justincschoi@gmail.com>
2023-11-03 18:09:59 -0400

    Render game board and add eventlisteners to field

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const gameBoard = (function () {
     const board = ['', '', '', '', '', '', '', '', ''];
 
+    const getBoard = () => {
+        return board;
+    }
+
     const setField = (index, sign) => {
         board[index] = sign;
     }
@@ -8,12 +12,45 @@ const gameBoard = (function () {
     const getField = (index) => {
         return board[index];
     }
+
+    return {
+        getBoard,
+        setField,
+        getField
+    }
 })();
 
 const user = function (sign) {
-    return { sign };
+    return {
+        sign
+    };
 }
 
 const displayController = (function () {
+    const boardContainer = document.querySelector('.board-container');
+    const fields = boardContainer.querySelectorAll('.field');
 
+    const renderGameBoard = () => {
+        fields.forEach((field, index) => {
+            field.textContent = gameBoard.getField(index);
+        })
+    };
+
+    fields.forEach((field, index) => {
+        field.addEventListener('click', () => {
+            gameBoard.setField(index, 'x');
+            renderGameBoard();
+        })
+    })
+
+    return {
+        renderGameBoard
+    }
 })();
+
+const gameController = (function () {
+    const player1 =
+
+        displayController.renderGameBoard();
+})();
+
